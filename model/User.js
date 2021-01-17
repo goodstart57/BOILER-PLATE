ccd64ae0a59cfdffa9801b53e2cbea4af5f65950
JAESEO LEE <goodstart57@gmail.com>
2021-01-18 00:01:13 +0900

    init

---
+++ model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true, // 문자열 내 입력된 공백 제거
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    // 유효성 관리
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
